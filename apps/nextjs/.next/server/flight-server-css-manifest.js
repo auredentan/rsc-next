8a25bbba5955e6e48592e98a4e411e73146ee248
Aurélien Dentan <aurelien.dentan@gmail.com>
2023-04-02 00:28:35 +0200

    d

--- apps/nextjs/.next/server/flight-server-css-manifest.js
+++ apps/nextjs/.next/server/flight-server-css-manifest.js
@@ -1,14 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "/home/auredentan/projects/rsc-next/apps/nextjs/app/page": [
-      "/home/auredentan/projects/rsc-next/apps/nextjs/app/globals.css"
-    ]
-  },
-  "/home/auredentan/projects/rsc-next/apps/nextjs/app/layout.tsx": [
-    "/home/auredentan/projects/rsc-next/apps/nextjs/app/globals.css"
-  ],
-  "/home/auredentan/projects/rsc-next/apps/nextjs/app/page.tsx": [
-    "/home/auredentan/projects/rsc-next/node_modules/.pnpm/next@13.2.4_biqbaboplfbrettd7655fr4n2y/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-    "/home/auredentan/projects/rsc-next/apps/nextjs/app/globals.css"
-  ]
-}
+self.__RSC_CSS_MANIFEST={"__entry_css_mods__":{"/home/auredentan/projects/rsc-next/apps/nextjs/app/page":["/home/auredentan/projects/rsc-next/apps/nextjs/app/globals.css"]},"/home/auredentan/projects/rsc-next/apps/nextjs/app/page.tsx":["/home/auredentan/projects/rsc-next/apps/nextjs/app/globals.css"],"/home/auredentan/projects/rsc-next/apps/nextjs/app/layout.tsx":["/home/auredentan/projects/rsc-next/apps/nextjs/app/globals.css"]}
