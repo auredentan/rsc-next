0631a3672e8af027d604a44cbe25df39bf3808d3
Aurélien Dentan <aurelien.dentan@gmail.com>
2023-04-04 20:44:59 +0200

    d

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -21,6 +21,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="My app"></meta>
         <Meta />
         <Links />
       </head>
